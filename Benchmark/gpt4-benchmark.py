d649a662160ecca2630837466a6db49934cc4d52
kendrick2324 <u202212045@hust.edu.cn>
2024-10-05 11:40:22 +0800

    Update

--- Benchmark/gpt4-benchmark.py
+++ Benchmark/gpt4-benchmark.py
@@ -1,90 +1,51 @@
 from openai import AzureOpenAI
 import time
-import fitz  # PyMuPDF
 import os
 import glob
+import fitz
 from pdfminer.high_level import extract_text
 import re
 
-# 定义关键词列表
-words_list = [
-    "benchmark", "evaluation", "performance", "comparison", "dataset", 
-    "improvement","metric", "experiment", "evaluate", "result","state-of-the-art","compare","judge",
-    "experimental results", "sota", "baseline"
-]
+def extract_all_text(pdf_path):
+    context = ""
+    with fitz.open(pdf_path) as pdf_file:
+        num_pages = pdf_file.page_count
 
-# 提取整个 PDF 的文本内容
-def extract_text_from_pdf(pdf_path):
-    text = extract_text(pdf_path)
-    return text
-
-# 按段落拆分文本内容
-def split_text_to_paragraphs(text):
-    # 使用两个换行符分割段落
-    paragraphs = re.split(r'\n\s*\n', text)
-    return paragraphs
-
-# 删除所有长度小于20的段落
-def remove_short_paragraphs(paragraphs):
-    filtered_paragraphs = []
-    for paragraph in paragraphs:
-        # 去除开头结尾的空白符，然后按空格分割单词列表
-        words = paragraph.strip().split()
-        # 只保留不等于1的段落
-        if len(words) >20:
-            filtered_paragraphs.append(paragraph.strip())
-    return filtered_paragraphs
-
-# 筛选含有关键词的段落
-def filter_paragraphs_by_keywords(paragraphs, keywords):
-    keyword_paragraphs = {}
-    i=1
-    for paragraph in paragraphs:
-        # 如果段落中包含任何一个关键词，则保留该段落
-        if any(keyword.lower() in paragraph.lower() for keyword in keywords):
-            keyword_paragraphs[i] = paragraph
-            i+=1
-    return keyword_paragraphs
-
-# 主函数
-def main(pdf_path):
-    # 提取 PDF 中的所有文本
-    text = extract_text_from_pdf(pdf_path)
-    # 将文本按段落拆分
-    paragraphs = split_text_to_paragraphs(text)
-    # 移除所有单个单词的段落
-    paragraphs = remove_short_paragraphs(paragraphs)
-    # 筛选出包含关键词的段落
-    keyword_paragraphs = filter_paragraphs_by_keywords(paragraphs, words_list)
-    # 将段落列表转换为一个字符串，每个段落之间用两个换行符分隔
-    return keyword_paragraphs
+        for page_num in range(num_pages):
+            page = pdf_file[page_num]
+            page_text = page.get_text()
+            context += page_text
+    return context
 
 def get_benchmark(para_content):
     if not para_content:
         return None
 
     prompt=prompt=f"""
-        Now I will give you some paragraphs of the paper for you to analyze and answer some questions. 
+        Now I will give you the content of the paper for you to analyze and answer some questions. 
         
         But first,I want you to know the following knowledge:
         
-        1.benchmark:A benchmark is a standard set of tests, including specific datasets , used to evaluate and compare the performance of different machine learning models or algorithms.
+        1.benchmark:A benchmark is a standardized dataset or task used to evaluate and compare the performance of different models, algorithms, or techniques . It needs to consist of both the test methods and test data.
         2.use llm to generate benchmark:This technique means using a large language model (LLM) to generate benchmarks for specific tasks or targets.The benchmarks generated must consist of specific datasets.These datasets must be processed by LLM or generated from scratch by LLM ,not person.
         3.use llm as a metric/judge:This technique means employing LLM to evaluate the quality or effectiveness of content, such as written work, code, or other outputs.It is often used in essay grading, code review, or creative writing critiques.
+        4.ethics statement:An ethic statement is a declaration that the research follows ethical guidelines, including consent, approval, privacy protection, and conflict of interest disclosure.
         
         Now Follow these steps for each paper:
         1.Check for benchmark usage: Does the paper use the existing benchmarks to evaluate the performance of the model or effectiveness of the new algorithm? 
         2.Create a new benchmark via LLM: Does the paper use the large language model to generate a new benchmark ?
         3.Use the LLM to evaluate: Does the paper use the large language model as a metric or judge to evaluate the quality or effectiveness of content?
-
-After considering the information, you should first answer the question "whether the paper uses the benchmark" with "yes" or "no". If the paper uses the benchmark, you should answer the question "how does the paper use the benchmark" with a summary of what benchmarks are used and show the results. Else you should answer that with "nothing".
-Then, you should answer the question "whether the paper generates a new benchmark via LLM" with "yes" or "no". If the paper generates a new benchmark via LLM, you should answer the question "how does the paper generate a new benchmark via LLM" with a summary of how does paper use the LLM to generate benchmark and why they use the LLM to generate.Else you should answer that with "nothing".
-Finally, you should answer the question "whether the paper use LLM as a judge/metric" with "yes" or "no". If the paper uses LLM a judge/metric, you should answer the question "how does the paper use LLM as a judge/metric" with a summary of what tasks does the paper use the LLM to evaluate and the reason why to use LLM.Else you should answer that with "nothing".
+        4.Give the ethics statement: Does the paper include an ethics statement?
+        
+After considering the information, you should first answer the question "whether the paper uses the benchmark" with "yes" or "no". If the paper uses the benchmark, you should answer the question "how does the paper use the benchmark" with a statement of all the benchmarks that are used and show the results in detail. Else you should answer that with "nothing".
+Then, you should answer the question "whether the paper generates a new benchmark via LLM" with "yes" or "no". If the paper generates a new benchmark via LLM, you should answer the question "how does the paper generate a new benchmark via LLM" with a statement of how does paper use the LLM to generate benchmark , why they use the LLM to generate and effect of the benchmark.Else you should answer that with "nothing".
+After that, you should answer the question "whether the paper use LLM as a judge/metric" with "yes" or "no". If the paper uses LLM a judge/metric, you should answer the question "how does the paper use LLM as a judge/metric" with a statement of what tasks does the paper use the LLM to evaluate , the reason why to use LLM and some other information.Else you should answer that with "nothing".
+Finally, you should answer the question "whether the paper includes an ethics statement" with "yes" or "no".If the paper includes an ethics statement, you should answer the question "the content of the ethics statement" with a summary of the content of the ethics statement.Else you should answer that with "nothing".
 Show your answers in JSON format like the Output format:
 
 Input format:
 {{
-    "Paragraph 1": the content of paragraph 1,"Paragraph 2": the content of paragraph 2,"Paragraph 3": the content of paragraph 3..."Paragraph n": the content of paragraph n
+    the content of the paper.
 }}
 
 Output format:
@@ -94,7 +55,9 @@ Output format:
   "whether the paper generates a new benchmark via LLM":yes/no,
   "how does the paper generate a new benchmark via LLM":"[how to create a new benchmark via LLM]",
   "whether the paper use LLM as a judge/metric":yes/no,
-  "how does the paper use LLM as a judge/metric":"[how to use LLM as a judge/metric]"
+  "how does the paper use LLM as a judge/metric":"[how to use LLM as a judge/metric]",
+  "whether the paper includes an ethics statement":yes/no,
+  "the content of the ethics statement":"[the content of the ethics statement]"
 }}
 
 Here is the information for the paper:{para_content}
@@ -126,13 +89,14 @@ Here is the information for the paper:{para_content}
             time.sleep(10)
 
     return output
+
 #"ICLR2024/paper/oral"
 pdf_path="temp"
 pdf_files = glob.glob(os.path.join(pdf_path, "*.pdf"))
 result_txt_path="result.txt"
 
 for pdf_file in pdf_files:
-    para_content=main(pdf_file)
+    para_content=extract_all_text(pdf_file)
     ans=get_benchmark(para_content)
     print(ans)
     with open(result_txt_path, 'a') as f:
@@ -146,7 +110,7 @@ for pdf_file in pdf_files:
 #     results[pdf_file] = ans
 #     count += 1
     
-#     if count % 1 == 0:
+#     if count % 10== 0:
 #         with open(result_path, 'a') as f:
 #             json.dump(results, f)
 #             f.write('\n')
